84908769c840a006b2bed3994fd4852e8ec02e3b
hamzaaslam-cs <160238839+hamzaaslam-etd@users.noreply.github.com>
2024-08-28 22:38:02 +0500

    redirection to products

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('products.index');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return redirect()->route('products.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -23,5 +23,3 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
